2ac8ff86a637ec9d4f04e396dc359888a4622f03
vantaylo <vanessalyn.taylor@gmail.com>
2026-04-21 19:25:03 -0500

    created image component for hero

---
+++ my-portfolio/src/Components/Layout/HeroImage.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function HeroImage() {
+  return (
+    <div className={classes.layer} aria-hidden="true">
+      <div className={classes.imageWrap}>
+        <Image
+          src="/youngMe.png"
+          alt=""
+          fill
+          priority
+          className={classes.image}
+        />
+      </div>
+    </div>
+  );
+}
+
+const classes = {
+  layer: `
+    pointer-events-none
+    absolute top-0 right-0
+    z-20
+    h-[100vw] max-h-[100svh] w-[50vw]
+    overflow-hidden
+  `,
+
+  imageWrap: "absolute inset-0",
+  image: "object-contain object-right-top select-none",
+};
